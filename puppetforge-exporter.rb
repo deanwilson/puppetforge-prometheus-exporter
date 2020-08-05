58a23496f940859e1f945013139b1b6ff8f317ae
Dean Wilson <dean.wilson@gmail.com>
2020-08-05 08:28:04 +0100

    Add metric for a module total download count
    Implemented as a counter as it should only go up.

--- puppetforge-exporter.rb
+++ puppetforge-exporter.rb
@@ -15,6 +15,13 @@ latest_downloads = Prometheus::Client::Gauge.new(
 )
 registry.register(latest_downloads)
 
+total_download_count = Prometheus::Client::Counter.new(
+  :puppetforge_module_downloads_total,
+  docstring: '...TODO...',
+  labels: %i[name module]
+)
+registry.register(total_download_count)
+
 user_names = ARGV.sort.uniq
 
 user_names.each do |user_name|
@@ -28,6 +35,9 @@ user_names.each do |user_name|
 
     downloads = releases[0].downloads
     latest_downloads.set(downloads, labels: { name: user_name, module: forge_module.name })
+
+    total_downloads = releases.map { |r| r.downloads }.sum
+    total_download_count.increment(by: total_downloads, labels: { name: user_name, module: forge_module.name })
   end
 end
 
